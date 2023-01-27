9f8951d48882629fe6facf92ced2032762888df3
Shane Dorsey <scdorsey22@gmail.com>
2023-01-27 14:06:04 -0800

    commit tree e2e67a1730e28b4a95e764ebad0192455fd1fa8b

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// code your solution here
+// const superbowlWin = function(record) {
+//     record.find([1] => [1].result === "W")
+//     return 
+// }
+
+// const superbowlWin = function(record) {
+//     const winYear = (record.find(win => win.result === "W"))
+//     return Object.values(winYear)[0]
+        
+
+
+// const superbowlWin = function(record) {
+//     for(let i = 0; record.length; i++) 
+//     {const winYear = (record.find(win => win.result === "W"))
+//     return Object.values(winYear)[i]}
+        
+// }
+
+
+// const superbowlWin = function(record) {
+//     for(let winYear of record) 
+//     {const winYear = (record.find(win => win.result === "W"))
+//     return Object.values(winYear)[i]}
+        
+// }
+
+const superbowlWin = function(record) {
+    const winYear = record.find(win => win.result === "W")
+    for (let key in winYear) {
+        return winYear[key]
+    }
+
+
+
+// }
+
+// function superbowlWin(record) {
+//     const winYear = record.find(function(result) {
+//         return result === "W"
+//     })
+//     return winYear
+}
